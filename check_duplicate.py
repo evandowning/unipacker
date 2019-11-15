05b00408ce5bd4a3635df98681efdeb38521c95a
Evan Downing <2077950+evandowning@users.noreply.github.com>
2019-11-15 20:16:25 +0000

    update check duplicate

--- check_duplicate.py
+++ check_duplicate.py
@@ -33,11 +33,34 @@ def _main():
             family[f]['total'] += 1
             family[f]['sha'].add(val)
 
+    famBeforeCheck = 0
+    totalBeforeCheck = 0
+    totalAfterCheck = 0
+
+    famAfterFilter = 0
+    totalAfterFilter = 0
+
     # Print out counts
     for k,v in sorted(family.items(), key=lambda x:x[1]['total'], reverse=True):
         # Focus on the families we care about
         if v['total'] > 10:
             sys.stdout.write('Family: {0}\tTotal: {1}\tUnique: {2}\n'.format(k,v['total'],len(v['sha'])))
+
+            famBeforeCheck += 1
+            totalBeforeCheck += v['total']
+            totalAfterCheck += len(v['sha'])
+
+            if len(v['sha']) > 10:
+                famAfterFilter += 1
+                totalAfterFilter += len(v['sha'])
+
+    sys.stdout.write('+++++++++++++++++++++++++++++\n')
+    sys.stdout.write('# families before check: {0}\n'.format(famBeforeCheck))
+    sys.stdout.write('# samples before check: {0}\n'.format(totalBeforeCheck))
+    sys.stdout.write('# samples after check: {0}\n'.format(totalAfterCheck))
+    sys.stdout.write('+++++++++++++++++++++++++++++\n')
+    sys.stdout.write('# families after check & filter: {0}\n'.format(famAfterFilter))
+    sys.stdout.write('# samples after check & filter: {0}\n'.format(totalAfterFilter))
 
 if __name__ == '__main__':
     _main()
